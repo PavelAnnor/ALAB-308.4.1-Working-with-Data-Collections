7a3a77ce9cea2e68bddb0833d6107499fb7a3910
PavelAnnor <annorpavel@gmail.com>
2026-05-13 15:57:28 -0400

    Modifed code to run more efficintly, more structured as well so you can see which program aligns with which problem

--- script.js
+++ script.js
@@ -1,66 +1,85 @@
-// console.log(`\nExcersise 3: Feeling Loopy`);
-const csv = "ID,Name,Occupation,Age\n42,Bruce,Knight,41\n57,Bob,Fry Cook,19\n63,Blaine,Quiz Master,58\n98,Bill,Doctor’s Assistant,26";
+//Part 2: Expanding Functionality
+console.log("\nPart 2 Expanding Functionality\n");
+const csv =
+  "ID,Name,Occupation,Age\n42,Bruce,Knight,41\n57,Bob,Fry Cook,19\n63,Blaine,Quiz Master,58\n98,Bill,Doctor’s Assistant,26";
+
 let arr = csv.split("\n"); //returns an array of strings from CSV, spilit up by '\n'
 
-let arr2 = [];
 //iterate through all the strings in arr
-for (i of arr){  
-   arr2.push( i.split(",")); //split converts intila strings from arr into array of strings split by ',', sotre them into new array
+for (i in arr) {
+  arr[i] = arr[i].split(","); //replace strings with same strings split into an array
 }
-// console.log(arr2);
+console.log(arr);
 
 //Part 3: Transforming Data
-let arr_of_objs = []; //empty array
-let keys = arr2[0]; //array of the keys we need
+console.log("\nPart 3 Transforming Data\n");
+let arr_of_objs = []; //empty array, gonna be our array of objects
 
-//for loop to create necessary amount of objects 
-for (let i = 1; i<arr2.length;i++){
-   
-    let obj = {}; //initilzing empty object
-    
-    for(let x=0;x<arr2[i].length;x++){
-        obj[keys[x].toLowerCase()] = arr2[i][x];
+/*create necessary amount of objects, we start at row i becasue the very first 
+ row in our 2D array is used for the properites of the object */
 
-    }
-    arr_of_objs.push(obj);
-    // console.log(obj);   
+//iterate through our 2d Array rows
+for (let row = 1; row < arr.length; row++) {
+  let obj = {}; //empty object we'll populate
+
+  //iterate throguh 2D array columns,
+  for (let col = 0; col < arr[row].length; col++) {
+    let property = arr[0][col].toLocaleLowerCase(); //get the name of the property from arr row 1(the properties row) column x
+    obj[property] = arr[row][col]; //the value is from arr row i, column x
+  }
+
+  arr_of_objs.push(obj); //push it into array
 }
-
 console.log(arr_of_objs);
 
 //Part 4 Sorting and Manipulating Data
-arr_of_objs.pop();
-
+console.log("\nPart 4 Sorting and Manipulating Data\n");
+console.log(`Our starting array is:`);
 console.log(arr_of_objs);
 
-arr_of_objs.splice(1,0, { id: "48", name: "Barry", occupation: "Runner", age: "25" });
+arr_of_objs.pop();
+console.log(`\nWhen we remove the last index from the sortred array we get:`);
+console.log(arr_of_objs);
 
+arr_of_objs.splice(1, 0, {
+  id: "48",
+  name: "Barry",
+  occupation: "Runner",
+  age: "25",
+});
+console.log(`\nWhen we add an object at index 1, we get:`);
 console.log(arr_of_objs);
 
 arr_of_objs.push({ id: "7", name: "Bilbo", occupation: "None", age: "111" });
+console.log(`\nWhen we add an object to the end, we get:`);
 console.log(arr_of_objs);
 
-
-//Part 5: Full Circle
-let str ="";
-for (c of arr_of_objs){
-    
-    for (const key in c ){
-        
-        str+=key +c[key];
-
-    }
-
+let average_age = 0;
+for (x of arr_of_objs) {
+  average_age += Number(x.age);
 }
 
+console.log(`The average age is ${average_age / arr_of_objs.length} years.`);
+
+// //Part 5: Full Circle
+console.log("\nPart 5 Full Circle\n");
+//output = "ID,Name,Occupation,Age\n42,Bruce,Knight,41\n57,Bob,Fry Cook,19\n63,Blaine,Quiz Master,58\n98,Bill,Doctor’s Assistant,26";
+
+let str = ""; //what ill return ultimatley
+let line_one = Object.keys(arr_of_objs[0]); //getting that first part of the string, the keys of the objects
+
+//go through each indivudal string in the array and turn it into its own array
+for (let i in line_one) {
+  line_one[i] = line_one[i].split(""); //turn it into an array
+  line_one[i][0] = line_one[i][0].toUpperCase(); //capitilze the first letter
+  line_one[i] = line_one[i].join(""); //join it back into a string
+}
+
+str += line_one.join(",") + "\n";
+
+//1)Loop throguh the indivual objects,
+for (let x of arr_of_objs) {
+  //x is the atcual object
+  str += Object.values(x).join(",") + "\n"; //2) get an array of their values, 3) collapse it into a string separated by comma
+}
 console.log(str);
-
-
-
-
-
-
-
-
-
-
